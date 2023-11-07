3d97fe20f10298de914eaf839765a5e5c6e6d187
Premgo <141892886+Premgo@users.noreply.github.com>
2023-11-06 18:25:49 -0800

    Create program1.cpp

---
+++ List_1/program1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b,sum=0;
+	cout<<"Prem Gohel";
+	cout<<"220130318003";
+	cout<<"enter the number:";
+	cin>>a;
+	cout<<"enter the second number:";
+	cin>>b;
+	
+	sum=a+b;
+	
+	cout<<"sum of numbers is:"<<sum<<endl;
+	return 0;
+}
